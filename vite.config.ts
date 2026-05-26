2dbc18c76de8171ed9330d68e9b068522543c944
Yuzhiran Dev <dev@yuzhiran.com>
2026-05-26 18:41:39 +0800

    vite.config 添加 VITE_BASE_PATH 支持子目录构建

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/',
   server: { port: 1420, host: '0.0.0.0' },
 })
